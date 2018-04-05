17024004cf13df5897d5ce0d3ecfb70cc7c469c9
unknown <jlramos97@gmail.com>
2018-04-05 14:11:53 +0200

    Arreglo fallos y creo ID_winner

--- database.sql
+++ database.sql
@@ -1,3 +1,9 @@
+drop table if exists 'Musician';
+drop table if exists 'Song';
+drop table if exists 'Challenge';
+drop table if exists 'Comment';
+drop table if exists 'Participation';
+
 create table Musician (
 	ID int auto_increment primary key,
 	username varchar(15),
@@ -20,22 +26,29 @@ create table Challenge (
 	ID int auto_increment primary key,
 	name varchar(25),
 	ID_song int,
+	ID_musician int,
+	ID_winner int default 0,
 	creat_date date,
 	fin_date date,
 	descr text,
-	foreign key (ID_song) references Song(ID)
+	foreign key (ID_song) references Song(ID),
+	foreign key (ID_musician) references Musician(ID)
 );
 
 create table Comment (
 	ID int auto_increment primary key,
+	ID_musician int,
 	com_date date,
-	content text
+	content text,
+	foreign key (ID_musician) references Musician(ID)
 );
 
 create table Participation(
-	ID_user int,
+	ID_musician int,
 	ID_chall int,
 	part_date date,
 	votes int default 0,
-	constraint PK_participation primary key (id_user, id_chall)
+	foreign key (ID_musician) references Musician(ID),
+	foreign key (ID_chall) references Challenge(ID),
+	constraint PK_participation primary key (id_musician, id_chall)
 );
