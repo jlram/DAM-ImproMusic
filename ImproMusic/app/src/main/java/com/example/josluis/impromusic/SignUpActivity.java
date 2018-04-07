8a15dba8bedd8cf51cefbd1849b8f3c49664eef3
José Luis <jlramos97@gmail.com>
2018-04-07 16:41:08 +0200

    TODO-> SignUpActivity no coge Object

--- ImproMusic/app/src/main/java/com/example/josluis/impromusic/SignUpActivity.java
+++ ImproMusic/app/src/main/java/com/example/josluis/impromusic/SignUpActivity.java
@@ -10,7 +10,20 @@ import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 import org.w3c.dom.Text;
+
+import java.util.Date;
 
 public class SignUpActivity extends AppCompatActivity {
 
@@ -26,6 +39,11 @@ public class SignUpActivity extends AppCompatActivity {
 
     Button mBtnSignUp;
 
+    String URLConsulta;
+    CharSequence fecha;
+
+    JsonObjectRequest consulta;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -40,10 +58,64 @@ public class SignUpActivity extends AppCompatActivity {
         mEditTextPWD2 = (EditText) findViewById(R.id.editTextPWD2Reg);
         mBtnSignUp = (Button) findViewById(R.id.buttonSignUp);
 
+        final RequestQueue queue = Volley.newRequestQueue(this);
+
         mBtnSignUp.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Toast.makeText(SignUpActivity.this, "Sign UP", Toast.LENGTH_SHORT).show();
+
+                /**
+                 * Comprueba que los campos de nombre y contraseña no están vacios para continuar.
+                 */
+                if(mEditTextUser.getText().equals("")) {
+                    Toast.makeText(SignUpActivity.this, "Rellena ambos campos", Toast.LENGTH_SHORT).show();
+
+                } else {
+
+                    android.text.format.DateFormat df = new android.text.format.DateFormat();
+
+                    fecha = df.format("yyyy-MM-dd", new Date());
+
+                    URLConsulta = "http://10.0.2.2/API_JSON/usuarios.php?accion=insertar&username=" +
+                            mEditTextUser.getText() +"&password=" + mEditTextPWD.getText() + "&logdate = " + fecha;
+                    /**
+                    TODOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOO4HEad
+                     */
+                    consulta = new JsonObjectRequest(Request.Method.GET, URLConsulta, null, new Response.Listener<JSONObject>() {
+
+                        @Override
+
+                        public void onResponse(JSONObject response) {
+                            try {
+
+                                boolean registrado = Boolean.parseBoolean(response.getString("estado"));
+
+                                if (registrado) {
+                                    Toast.makeText(SignUpActivity.this, "gogogo", Toast.LENGTH_SHORT).show();
+                                } else {
+                                    Toast.makeText(SignUpActivity.this, "????????????????", Toast.LENGTH_SHORT).show();
+                                }
+
+                            } catch (JSONException e) {
+                                e.printStackTrace();
+                                Toast.makeText(SignUpActivity.this, "lmao", Toast.LENGTH_SHORT).show();
+                            }
+                        }
+                    }, new Response.ErrorListener() {
+
+                        @Override
+                        public void onErrorResponse(VolleyError error) {
+                            // TODO Auto-generated method stub
+                            Toast.makeText(SignUpActivity.this, "Error " + error.getMessage(),
+                                    Toast.LENGTH_SHORT).show();
+                        }
+                    });
+                }
+                //Añade la consulta a la RequestQueue
+                queue.add(consulta);
+
+
             }
         });
 
