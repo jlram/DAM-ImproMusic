f6be44276ad289545a571db2c7fbb40fb3ce0114
José Luis <jlramos97@gmail.com>
2018-04-22 13:50:12 +0200

    Diseñada SongActivity y creada CreateChallengeActivity.

--- ImproMusic/app/src/main/java/com/example/josluis/impromusic/SongActivity.java
+++ ImproMusic/app/src/main/java/com/example/josluis/impromusic/SongActivity.java
@@ -1,7 +1,12 @@
 package com.example.josluis.impromusic;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.SeekBar;
 import android.widget.TextView;
 
 import static com.example.josluis.impromusic.MainActivity.cancion;
@@ -9,6 +14,15 @@ import static com.example.josluis.impromusic.MainActivity.cancion;
 public class SongActivity extends AppCompatActivity {
 
     TextView textViewCancion;
+    TextView textViewInfo;
+
+    ImageButton buttonPlay;
+    SeekBar seekBar;
+
+    Button buttonCrearReto;
+    Button buttonVerRetos;
+
+    boolean play = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,7 +30,36 @@ public class SongActivity extends AppCompatActivity {
         setContentView(R.layout.activity_song);
 
         textViewCancion = (TextView) findViewById(R.id.textViewCancion);
+        textViewInfo = (TextView) findViewById(R.id.textViewInfo);
+
+        buttonCrearReto = (Button) findViewById(R.id.buttonCrearReto);
+        buttonVerRetos = (Button) findViewById(R.id.buttonVerRetos);
+
+        buttonPlay = (ImageButton) findViewById(R.id.imageButtonMp3);
+        seekBar = (SeekBar)   findViewById(R.id.seekBarCancion);
 
         textViewCancion.setText(cancion.getName());
+
+        textViewInfo.setText("Artista: " + cancion.getAuthor() + "\nAlbum: " + cancion.getAlbum());
+
+        buttonPlay.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+               if (play) {
+                   buttonPlay.setImageResource(android.R.drawable.ic_media_play);
+                   play = false;
+               } else {
+                   buttonPlay.setImageResource(android.R.drawable.ic_media_pause);
+                   play = true;
+               }
+            }
+        });
+
+        buttonCrearReto.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(SongActivity.this, CreateChallengeActivity.class));
+            }
+        });
     }
 }
