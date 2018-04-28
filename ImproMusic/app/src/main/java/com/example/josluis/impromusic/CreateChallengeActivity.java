310532dd78f9ee2fe1dce8c73d43c2088eb00ccb
José Luis <jlramos97@gmail.com>
2018-04-28 12:10:29 +0200

    Añadido el spinner en CreateChallengeActivity y evento onClick de fab

--- ImproMusic/app/src/main/java/com/example/josluis/impromusic/CreateChallengeActivity.java
+++ ImproMusic/app/src/main/java/com/example/josluis/impromusic/CreateChallengeActivity.java
@@ -1,12 +1,24 @@
 package com.example.josluis.impromusic;
 
+import android.os.Build;
+import android.support.annotation.RequiresApi;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.CalendarView;
 import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.example.josluis.impromusic.Adapters.MainAdapter;
+
+import java.util.ArrayList;
+
+import static com.example.josluis.impromusic.LoginActivity.usuario;
+import static com.example.josluis.impromusic.MainActivity.listaCanciones;
 
 public class CreateChallengeActivity extends AppCompatActivity {
 
@@ -16,6 +28,8 @@ public class CreateChallengeActivity extends AppCompatActivity {
     EditText editTextDescrChall;
 
     Spinner spinnerChall;
+    ArrayAdapter<String> adapter;
+    ArrayList<String> arraySpinner;
     CalendarView calendarViewChall;
 
     FloatingActionButton fabChall;
@@ -36,5 +50,51 @@ public class CreateChallengeActivity extends AppCompatActivity {
 
         fabChall = (FloatingActionButton) findViewById(R.id.floatingActionButtonChall);
 
+        arraySpinner = new ArrayList<>();
+
+        rellenaSpinner();
+
+        /**
+         * Botón que nos hará ir al siguiente fragment.
+         *
+         *   String nombre = editTextNombreChall.getText().toString();
+         *  String descripcion = editTextDescrChall.getText().toString();
+         *   int ID_musc = usuario.getID();
+         *
+         */
+        fabChall.setOnClickListener(new View.OnClickListener() {
+            @RequiresApi(api = Build.VERSION_CODES.N)
+            @Override
+            public void onClick(View view) {
+
+            //TODO -> CAMBIAR DE FRAGMENT
+                Toast.makeText(CreateChallengeActivity.this, "cambio de fragment"
+                        , Toast.LENGTH_SHORT).show();
+
+            }
+        });
+
     }
+
+    /**
+     * Recorremos todos los nombres de canciones y vamos introduciendo sus nombres
+     * de manera dinámica.
+     *
+     * Una vez hecho esto, creamos un adapter para nuestro spinner, le asignamos
+     * una vista para ver los items de una manera más clara y lo asignamos
+     * a nuestro spinner.
+     */
+    public void rellenaSpinner() {
+        for(int i = 0; i < listaCanciones.size(); i++) {
+            arraySpinner.add(listaCanciones.get(i).getName());
+        }
+
+        adapter = new ArrayAdapter<>(
+                this, android.R.layout.simple_spinner_item, arraySpinner);
+
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+        spinnerChall.setAdapter(adapter);
+    }
+
 }
