1f8bc4602218275f100d121888418fa15992a1b9
José Luis <jlramos97@gmail.com>
2018-05-05 18:52:55 +0200

    Se introduce correctamente la fecha de caducidad del reto.

--- ImproMusic/app/src/main/java/com/example/josluis/impromusic/CreateChallengeActivity.java
+++ ImproMusic/app/src/main/java/com/example/josluis/impromusic/CreateChallengeActivity.java
@@ -1,5 +1,7 @@
 package com.example.josluis.impromusic;
 
+import android.annotation.SuppressLint;
+import android.icu.util.Calendar;
 import android.os.Build;
 import android.os.Bundle;
 import android.support.annotation.RequiresApi;
@@ -36,6 +38,8 @@ public class CreateChallengeActivity extends AppCompatActivity {
     EditText editTextDescrChall;
 
     Spinner spinnerChall;
+    int selected = 0;
+
     Spinner spinnerDate;
 
     ArrayAdapter<String> adapter;
@@ -101,13 +105,16 @@ public class CreateChallengeActivity extends AppCompatActivity {
     public void rellenaSpinners() {
         for(int i = 0; i < listaCanciones.size(); i++) {
             arraySpinner.add(listaCanciones.get(i).getName());
+            if (listaCanciones.get(i).getID() == cancion.getID()) {
+                selected = i;
+            }
         }
 
         adapter = new ArrayAdapter<>(
                 this, android.R.layout.simple_spinner_item, arraySpinner);
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         spinnerChall.setAdapter(adapter);
-
+        spinnerChall.setSelection(selected);
 
         adapterFecha = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item,
                 getResources().getStringArray(R.array.fechas));
@@ -115,20 +122,60 @@ public class CreateChallengeActivity extends AppCompatActivity {
         spinnerDate.setAdapter(adapterFecha);
 
     }
+
     /**
-     * Comprobamos que los campos de texto no estén ocupados, sustituimos los espacios por "-"
-     * para que no se altere la URL y creamos una consulta.
+     * Comprueba el texto del spinner para así crera un objeto Calendar con la fecha actual
+     * y sumarle el numero de dias correspondiente para así establecer una fecha de fin
+     * del reto.
+     * @return
+     */
+    @SuppressLint("NewApi")
+    public CharSequence cogeSpinner() {
+
+        Calendar c = Calendar.getInstance();
+        c.setTime(new Date());
+        switch (spinnerDate.getSelectedItem().toString()) {
+            case "24 Horas" :
+                c.add(Calendar.DATE, 1);
+                break;
+            case "3 Días"   :
+                c.add(Calendar.DATE, 3);
+                break;
+            case  "1 Semana":
+                c.add(Calendar.DATE, 7);
+                break;
+            case "1 Mes"    :
+                c.add(Calendar.DATE, 30);
+                break;
+            case "Elige una duración..." :
+                return "ERROR";
+        }
+
+        Date fechaSpinner = c.getTime();
+        android.text.format.DateFormat df = new android.text.format.DateFormat();
+        CharSequence fecha_fin = df.format("yyyy-MM-dd", fechaSpinner);
+
+        return fecha_fin;
+
+    }
+
+    /**
+     * Comprobamos que los campos de texto no estén ocupados, al igual que con los spinners.
+     * En caso de que estén todos los campos correctos, hacemos llamada a la base de datos y
+     * creamos un nuevo reto.
+     * TODO -> Comprobación de los editText
      */
     public void creaReto() {
-        if (!editTextNombreChall.getText().toString().trim().equals("") ||
-                !editTextDescrChall.getText().toString().trim().equals("")) {
-            android.text.format.DateFormat df = new android.text.format.DateFormat();
-            CharSequence fecha_fin = df.format("yyyy-MM-dd", new Date());
+        CharSequence fecha2 = cogeSpinner();
+        Toast.makeText(this, fecha2, Toast.LENGTH_SHORT).show();
+            if (!isEmpty(editTextNombreChall) || //editTextNombreChall != null || editTextNombreChall.length() != 0 ||
+                !isEmpty(editTextDescrChall) || //editTextDescrChall != null || editTextDescrChall.length() != 0 ||
+                !fecha2.equals("ERROR")) {
 
             URLConsulta = "http://10.0.2.2/API_JSON/usuarios.php?accion=crearReto&name=" +
                     editTextNombreChall.getText().toString() + "&id_song=" + cancion.getID() +
                     "&id_user=" + usuario.getID() + "&creat_date=" + fecha + "&fin_date=" +
-                    fecha_fin + "&descr=" + editTextDescrChall.getText().toString();
+                    fecha2 + "&descr=" + editTextDescrChall.getText().toString();
 
             consulta = new JsonObjectRequest(Request.Method.GET, URLConsulta, null,
                 new Response.Listener<JSONObject>() {
@@ -147,8 +194,18 @@ public class CreateChallengeActivity extends AppCompatActivity {
             });
             queue.add(consulta);
         } else {
-            Toast.makeText(this, "Por favor, rellena los campos correctamente",
-                    Toast.LENGTH_SHORT).show();
+            if (fecha2.equals("ERROR")) {
+                Toast.makeText(this, "Por favor, elige una fecha.",
+                        Toast.LENGTH_SHORT).show();
+            } else {
+                Toast.makeText(this, "Por favor, rellena los campos correctamente",
+                        Toast.LENGTH_SHORT).show();
+            }
         }
     }
+
+    private boolean isEmpty(EditText etText) {
+        return etText.getText().toString().trim().length() == 0;
+    }
+
 }
