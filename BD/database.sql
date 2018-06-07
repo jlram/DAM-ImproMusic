1f3b96af23ccb9c3c7d7fe7c9d954b138f4f049c
José Luis <jlramos97@gmail.com>
2018-06-08 00:03:07 +0200

    Update en la base de datos, nueva tabla vote, y nuevas funciones en usuarios.php

--- BD/database.sql
+++ BD/database.sql
@@ -1,73 +1,157 @@
-drop table if exists `Musician`;
-drop table if exists `Song`;
-drop table if exists `Challenge`;
-drop table if exists `Comment`;
-drop table if exists `Participation`;
+-- phpMyAdmin SQL Dump
+-- version 4.7.4
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1:3306
+-- Generation Time: Jun 03, 2018 at 04:19 PM
+-- Server version: 5.7.19
+-- PHP Version: 5.6.31
 
-create table Musician (
-	ID int auto_increment primary key,
-	username varchar(15),
-	password varchar(15),
-	log_date date,
-	user_type varchar(10),
-	id_pic int
-);
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
 
-create table Song (
-	ID int auto_increment primary key,
-	name varchar(25),
-	author varchar(25),
-	album varchar(25),
-	genre varchar(15),
-	duration int,
-	link varchar(50)
-);
 
-create table Challenge (
-	ID int auto_increment primary key,
-	name varchar(25),
-	ID_song int,
-	ID_musician int,
-	ID_winner int default 0,
-	creat_date date,
-	fin_date date,
-	descr text,
-	foreign key (ID_song) references Song(ID),
-	foreign key (ID_musician) references Musician(ID)
-);
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
 
-create table Comment (
-	ID int auto_increment primary key,
-	ID_musician int,
-	com_date date,
-	content text,
-	foreign key (ID_musician) references Musician(ID)
-);
+--
+-- Database: `impromusic`
+--
 
-create table Participation(
-	ID_musician int,
-	ID_chall int,
-	part_date date,
-	votes int default 0,
-	youtube varchar(50),
-	foreign key (ID_musician) references Musician(ID),
-	foreign key (ID_chall) references Challenge(ID),
-	constraint PK_participation primary key (id_musician, id_chall)
-);
+-- --------------------------------------------------------
 
-insert into `Musician` values (null, 'admin', 'admin', '2018-04-05', 'admin', '2');
-insert into `Musician` values (2, 'invitado', 'invitado', '2018-04-09', 'invitado', '1');
+--
+-- Table structure for table `challenge`
+--
 
-insert into `Song` values (null, 'Cancion 1', 'Autor 1', 'Album 1', 'Rock', 3, "");
-insert into `Song` values (null, 'Cancion 2', 'Autor 2', 'Album 2', 'Pop', 4, "");
-insert into `Song` values (null, 'Cancion 3', 'Autor 3', 'Album 3', 'Rock', 3, "");
-insert into `Song` values (null, 'Cancion 4', 'Autor 5', 'Album 5', 'Pop', 2, "");
-insert into `Song` values (null, 'Cancion 5', 'Autor 2', 'Album 2', 'Rock', 8, "");
-insert into `Song` values (null, 'Cancion 6', 'Autor 4', 'Album 4', 'Pop', 3, "");
-insert into `Song` values (null, 'Cancion 7', 'Autor 6', 'Album 6', 'Rock', 5, "");
-insert into `Song` values (null, 'Cancion 8', 'Autor 1', 'Album 1', 'Pop', 10, "");
-insert into `Song` values (null, 'Cancion 9', 'Autor 4', 'Album 4', 'Rock', 5, "");
-insert into `Song` values (null, 'Cancion 10', 'Autor 6', 'Album 6', 'Pop', 6, "");
-insert into `Song` values (null, 'Cancion 11', 'Autor 3', 'Album 3', 'Rock', 3, "");
-insert into `Song` values (null, 'Cancion 12', 'Autor 2', 'Album 2', 'Pop', 4, "");
-insert into `Song` values (null, 'BrotherMan BIll', 'TerribleTim', 'Spreads Love', 'Country', 4, "https://jlramos97.github.io/music/brotherman.mp3");
+DROP TABLE IF EXISTS `challenge`;
+CREATE TABLE IF NOT EXISTS `challenge` (
+  `ID` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(25) DEFAULT NULL,
+  `ID_song` int(11) DEFAULT NULL,
+  `ID_musician` int(11) DEFAULT NULL,
+  `ID_winner` int(11) DEFAULT '0',
+  `creat_date` date DEFAULT NULL,
+  `fin_date` date DEFAULT NULL,
+  `descr` text,
+  PRIMARY KEY (`ID`),
+  KEY `ID_song` (`ID_song`),
+  KEY `ID_musician` (`ID_musician`)
+) ENGINE=MyISAM AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `challenge`
+--
+
+INSERT INTO `challenge` (`ID`, `name`, `ID_song`, `ID_musician`, `ID_winner`, `creat_date`, `fin_date`, `descr`) VALUES
+(1, '1234234', 13, 2, NULL, '2018-05-24', '2018-05-27', '1234123412341234');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `comment`
+--
+
+DROP TABLE IF EXISTS `comment`;
+CREATE TABLE IF NOT EXISTS `comment` (
+  `ID` int(11) NOT NULL AUTO_INCREMENT,
+  `ID_musician` int(11) DEFAULT NULL,
+  `com_date` date DEFAULT NULL,
+  `content` text,
+  PRIMARY KEY (`ID`),
+  KEY `ID_musician` (`ID_musician`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `musician`
+--
+
+DROP TABLE IF EXISTS `musician`;
+CREATE TABLE IF NOT EXISTS `musician` (
+  `ID` int(11) NOT NULL AUTO_INCREMENT,
+  `username` varchar(15) DEFAULT NULL,
+  `password` varchar(15) DEFAULT NULL,
+  `log_date` date DEFAULT NULL,
+  `user_type` varchar(10) DEFAULT NULL,
+  `id_pic` int(11) DEFAULT NULL,
+  PRIMARY KEY (`ID`)
+) ENGINE=MyISAM AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `musician`
+--
+
+INSERT INTO `musician` (`ID`, `username`, `password`, `log_date`, `user_type`, `id_pic`) VALUES
+(1, 'admin', 'admin', '2018-04-05', 'admin', 2),
+(2, 'invitado', 'invitado', '2018-04-09', 'invitado', 1);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `participation`
+--
+
+DROP TABLE IF EXISTS `participation`;
+CREATE TABLE IF NOT EXISTS `participation` (
+  `ID` int 11 NOT NULL AUTO_INCREMENT,
+  `ID_musician` int(11) NOT NULL,
+  `ID_chall` int(11) NOT NULL,
+  `part_date` date DEFAULT NULL,
+  `votes` int(11) DEFAULT '0',
+  `youtube` varchar(50) DEFAULT NULL,
+  PRIMARY KEY (`ID_musician`,`ID_chall`),
+  KEY `ID_chall` (`ID_chall`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+
+DROP TABLE IF EXISTS `vote`;
+
+CREATE TABLE `vote` (
+  `ID_part` int(15) NOT NULL,
+  `ID_musician` int(15) NOT NULL,
+  `ID_chall` int(15) NOT NULL,
+    PRIMARY KEY (`ID_musician`,`ID_chall`),
+  KEY `ID_chall` (`ID_chall`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `song`
+--
+
+DROP TABLE IF EXISTS `song`;
+CREATE TABLE IF NOT EXISTS `song` (
+  `ID` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(25) DEFAULT NULL,
+  `author` varchar(25) DEFAULT NULL,
+  `album` varchar(25) DEFAULT NULL,
+  `genre` varchar(15) DEFAULT NULL,
+  `cover` varchar(100) DEFAULT NULL,
+  `link` varchar(100) DEFAULT NULL,
+  PRIMARY KEY (`ID`)
+) ENGINE=MyISAM AUTO_INCREMENT=14 DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `song`
+--
+
+INSERT INTO `song` (`ID`, `name`, `author`, `album`, `genre`, `cover`, `link`) VALUES
+(1, 'Funk', 'Boyish Giggles', 'Funk & Soul', 'Funk', 'http://hyperbruh.000webhostapp.com/music/funk.jpg', 'http://hyperbruh.000webhostapp.com/music/funk.mp3'),
+(2, 'Post-Rock', 'milesov', 'Pira', 'Post-Rock', 'http://hyperbruh.000webhostapp.com/music/post.jpg', 'http://hyperbruh.000webhostapp.com/music/post.mp3'),
+(3, 'Blues', 'DansGame', 'The Blues Album', 'Blues', 'http://hyperbruh.000webhostapp.com/music/blues.jpg', 'http://hyperbruh.000webhostapp.com/music/blues.mp3'),
+(4, 'Psicodelia', 'TriHard', 'KKorner', 'Psicodelia', 'http://hyperbruh.000webhostapp.com/music/psicodelia.jpg', 'http://hyperbruh.000webhostapp.com/music/psicodelia.mp3'),
+(5, 'Half Sister', 'Protomartyr', 'Relatives in Descent', 'Shoegaze', 'http://hyperbruh.000webhostapp.com/music/relatives.jpg', 'http://hyperbruh.000webhostapp.com/music/halfsister.mp3'),
+(6, 'BrotherMan BIll', 'TerribleTim', 'Spreads Love', 'Country', 'http://hyperbruh.000webhostapp.com/music/brotherman.jpg', 'http://hyperbruh.000webhostapp.com/music/brotherman.mp3'),
+(7, 'Comfortably Numb', 'Pink Floyd', 'The Wall', 'Rock', 'http://hyperbruh.000webhostapp.com/music/thewall.jpg', 'http://hyperbruh.000webhostapp.com/music/comfortably.mp3');
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
--- BD/usuarios.php
+++ BD/usuarios.php
@@ -5,7 +5,7 @@ function Conectar($host,$user,$pass,$dbname)
 {
    $conexion=mysqli_connect($host,$user,$pass) or die("ERROR CONEXION");
    mysqli_select_db($conexion,$dbname) or die("ERROR CONEXION");
-   
+
    return $conexion;
 }
 
@@ -30,20 +30,27 @@ $conexion=Conectar($host,$user,$pass,$dbname);
    switch($opcion)
    {
       /*CONSULTAS DE LA BASE DE DATOS DE USUARIOS*/
-      case "consulta":
-          $query="SELECT * FROM Musician";
-          $resultado=mysqli_query($conexion,$query) or die("ERROR");
-          $json=TABLA_A_JSON($resultado);
-          echo $json;
-      break;
+    	case "consulta":
+          	$query="SELECT * FROM musician";
+          	$resultado=mysqli_query($conexion,$query) or die("ERROR");
+          	$json=TABLA_A_JSON($resultado);
+          	echo $json;
+    	break;
+
+    	case "consultaPorID":
+    		$id=$_GET["id"];
+          	$query="SELECT * FROM musician WHERE ID = '$id'";
+          	$resultado=mysqli_query($conexion,$query) or die("ERROR");
+          	$json=TABLA_A_JSON($resultado);
+          	echo $json;
+    	break;
 
       case "login":
           $user=$_GET["username"];
           $pass=$_GET["password"];
-          $query="SELECT * FROM Musician where username ='$user' and password = '$pass' ";
+          $query="SELECT * FROM musician where username ='$user' and password = '$pass' ";
           $resultado=mysqli_query($conexion,$query) or die("ERROR");
           $json=TABLA_A_JSON($resultado);
-
           echo $json;
       break;
 
@@ -51,7 +58,7 @@ $conexion=Conectar($host,$user,$pass,$dbname);
            $user=$_GET["username"];
            $pass=$_GET["password"];
            $logdate=$_GET["logdate"];
-           $query="INSERT INTO Musician(ID,username,password,log_date,user_type, id_pic) values (null,'$user','$pass','$logdate','normal','1');";
+           $query="INSERT INTO musician(ID,username,password,log_date,user_type, id_pic) values (null,'$user','$pass','$logdate','normal','1');";
            mysqli_query($conexion,$query) or die("ERROR");
            echo '{"estado":true}';
       break;
@@ -62,23 +69,49 @@ $conexion=Conectar($host,$user,$pass,$dbname);
            mysqli_query($conexion,$query) or die("ERROR");
            echo '{"estado":true}';
       break;
-      
+
       case "actualizar":
            $user=$_GET["username"];
            $pass=$_GET["password"];
-           $query="UPDATE Musician SET password='$pass' WHERE username='$user';";
+           $query="UPDATE musician SET password='$pass' WHERE username='$user';";
            mysqli_query($conexion,$query) or die("ERROR");
            echo '{"estado":true}';
       break;
 
-
-
       /*CONSULTAS DE LA TABLA CANCIONES*/
       case "consultaCanciones":
-          $query="SELECT * FROM Song";
+          $query="SELECT * FROM song";
           $resultado=mysqli_query($conexion,$query) or die("ERROR");
           $json=TABLA_A_JSON($resultado);
           echo $json;
+      break;
+
+      case "consultaCancionPorID":
+      	  $song=$_GET["song"];
+          $query="SELECT * FROM song WHERE ID = '$song'";
+          $resultado=mysqli_query($conexion,$query) or die("ERROR");
+          $json=TABLA_A_JSON($resultado);
+          echo $json;
+      break;
+
+       case "meterCancion":
+           $name=$_GET["name"];
+           $author=$_GET["author"];
+           $album=$_GET["album"];
+           $genre=$_GET["genre"];
+           $cover=$_GET["cover"];
+           $link=$_GET["link"];
+
+           $query="INSERT INTO `song` (`ID`, `name`, `author`, `album`, `genre`, `cover`, `link`) VALUES (NULL, '$name', '$author', '$album', '$genre', '$cover', '$link');";
+           $resultado=mysqli_query($conexion,$query) or die("ERROR");
+           echo '{"estado":true}';
+      break;
+
+      case "eliminarCancion":
+          $id_song=$_GET["id_song"];
+          $query="DELETE FROM song WHERE ID = '$id_song'";
+          mysqli_query($conexion,$query) or die("ERROR");
+           echo '[{"estado":true}]';
       break;
 
 
@@ -90,37 +123,93 @@ $conexion=Conectar($host,$user,$pass,$dbname);
           $creat_date=$_GET["creat_date"];
           $fin_date=$_GET["fin_date"];
           $descr=$_GET["descr"];
-          $query="INSERT INTO Challenge values (null, '$name',$id_song,$id_user, null,'$creat_date','$fin_date', '$descr');";
+          $query="INSERT INTO challenge values (null, '$name',$id_song,$id_user, null,'$creat_date','$fin_date', '$descr');";
            mysqli_query($conexion,$query) or die("ERROR");
            echo '{"estado":true}';
       break;
 
       case "consultaRetos":
-          $query="SELECT * FROM Challenge";
+          $query="SELECT * FROM challenge";
           $resultado=mysqli_query($conexion,$query) or die("ERROR");
           $json=TABLA_A_JSON($resultado);
           echo $json;
+      break;
+
+      case "consultaRetosPorID":
+        $id_song=$_GET["id_song"];
+        $query="SELECT * FROM challenge WHERE ID_song = '$id_song'";
+        $resultado=mysqli_query($conexion,$query) or die("ERROR");
+        $json=TABLA_A_JSON($resultado);
+        echo $json;
       break;
 
       /*CONSULTA DE LA TABLA PARTICIPACIONES*/
 
       case "consultaPart":
-          $query="SELECT * FROM Participation";
+          $reto=$_GET["reto"];
+          $query="SELECT * FROM participation WHERE ID_Chall = '$reto' ORDER BY `participation`.`votes` DESC ";
           $resultado=mysqli_query($conexion,$query) or die("ERROR");
           $json=TABLA_A_JSON($resultado);
           echo $json;
       break;
 
+      /*CONSULTAS DE LAS TABLAS PARTICIPATION Y VOTO*/
 
-      // case "consultaPartCancion":
-      //     $reto=$_GET["reto"];
-      //     $query="SELECT * FROM Participation WHERE ID_Chall = '$reto'";
-      //     $resultado=mysqli_query($conexion,$query) or die("ERROR");
-      //     $json=TABLA_A_JSON($resultado);
-      //     echo $json;
-      // break;
+      case "registrarParticipacion":
+          $chall=$_GET["chall"];
+          $music=$_GET["music"];
+          $date=$_GET["date"];
+          $youtube=$_GET["youtube"];
+          $query="INSERT INTO `participation` (`ID_musician`, `ID_chall`, `part_date`, `votes`, `youtube`) VALUES ('$music', '$chall', '$date', '0', '$youtube');";
+          mysqli_query($conexion,$query) or die("ERROR");
+           echo '[{"estado":true}]';
+      break;
 
+      case "sumarVoto":
+          // $chall=$_GET["chall"];
+          // $music=$_GET["music"];
+          $part=$_GET["part"];
+          $query="UPDATE participation SET votes = votes + 1 where ID = '$part'";
+          mysqli_query($conexion,$query) or die("ERROR");
+           echo '[{"estado":true}]';
+      break;
+
+      case "registrarVoto":
+          $chall=$_GET["chall"];
+          $music=$_GET["music"];
+          $part=$_GET["part"];
+          $query="INSERT INTO vote values ('$part', '$music', '$chall');";
+          mysqli_query($conexion,$query) or die("ERROR");
+           echo '[{"estado":true}]';
+      break;
+
+      case "restarVoto":
+          // $chall=$_GET["chall"];
+          // $music=$_GET["music"];
+          $part=$_GET["part"];
+          $query="UPDATE participation SET votes = votes - 1 where ID= '$part'";
+          mysqli_query($conexion,$query) or die("ERROR");
+           echo '[{"estado":true}]';
+      break;
+
+      case "eliminarVoto":
+          $chall=$_GET["chall"];
+          $music=$_GET["music"];
+          $part=$_GET["part"];
+          $query="DELETE FROM vote WHERE ID_musician = '$music' AND ID_chall = '$chall' AND ID_part = '$part'";
+          mysqli_query($conexion,$query) or die("ERROR");
+           echo '[{"estado":true}]';
+      break;
+
+      case "consultarVoto":
+          $chall=$_GET["chall"];
+          $music=$_GET["music"];
+          $part=$_GET["part"];
+          $query="SELECT * FROM vote WHERE ID_musician = '$music' AND ID_chall = '$chall' AND ID_part= '$part'";
+          $resultado=mysqli_query($conexion,$query) or die("ERROR");
+          $json=TABLA_A_JSON($resultado);
+          echo $json;
+      break;
     }
 
 ?>
-
